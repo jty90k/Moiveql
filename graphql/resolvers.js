0cc727131444d4091f7c41f9ae6c0081ba64138c
UniverseStark <jty90k@gmail.com>
2021-04-02 21:51:33 +0900

    Movie Suggestions

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,10 @@
-import { getMovies, getMovie } from "./db.js";
+import { getMovies, getMovie, getSuggestions } from "./db.js";
 
 const resolvers = {
   Query: {
     movies: (_, { rating, limit }) => getMovies(limit, rating),
     movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
